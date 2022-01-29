094450bb7b8ba369805a9ba2e858de310b93d3d6
Arunav Bhattacharya <itsarunav4u@gmail.com>
2022-01-28 20:31:20 -0700

    mutiny-multi

--- src/main/kotlin/me/arunav/mutiny/_2CreatingUnis.kt
+++ src/main/kotlin/me/arunav/mutiny/_2CreatingUnis.kt
@@ -1,24 +1,52 @@
 package me.arunav.mutiny
 
 import io.smallrye.mutiny.Uni
+import io.smallrye.mutiny.subscription.Cancellable
 
-fun creatingUnis() {
+/**
+ * Create Uni using item
+ */
+fun createUni(): Uni<Int> {
     val num = 10
     val item: Uni<Int> = Uni.createFrom().item(num)
-    val supplier = Uni.createFrom().item { num * 5 }
+    val item2 = Uni.createFrom().item { num * 5 } // Another way to create a Uni using a Supplier
+    return item
 }
 
+/**
+ *  Subscribe to a Uni
+ */
+fun subscribeToUni() {
+    val cancellable: Cancellable =
+        createUni()
+            .subscribe()
+            .with(
+                { println(it) },
+                { println("Failed with $it") }
+            )
+}
+
+/**
+ * Create failing Unis
+ */
 fun creatingFailingUnis() {
-    val exception = Uni.createFrom().failure<Int>(Exception("Invalid Int"))
-    val exceptionWithASupplier = Uni.createFrom().failure<Int> { Exception("Exception using Supplier") }
+    val exception: Uni<Int> = Uni.createFrom().failure(Exception("Invalid Int"))
+    val exception2 = Uni.createFrom().failure<Int> { Exception("Exception using Supplier") }
 }
 
+/**
+ * Create empty Unis
+ */
 fun creatingNullUni() {
     val nullItem = Uni.createFrom().nullItem<Int>()
 }
 
+/**
+ * Create Uni from an Emitter.
+ */
 fun creatingUniFromEmitter() {
-    Uni.createFrom().emitter<String> {
-        it.complete("result")
-    }
+    Uni.createFrom()
+        .emitter<String> {
+            it.complete("result")
+        }
 }
--- src/main/kotlin/me/arunav/mutiny/_3CreatingMultis.kt
+++ src/main/kotlin/me/arunav/mutiny/_3CreatingMultis.kt
@@ -1,1 +1,47 @@
 package me.arunav.mutiny
+
+import io.smallrye.mutiny.Multi
+import io.smallrye.mutiny.subscription.Cancellable
+import java.time.Duration
+import java.util.stream.IntStream
+
+// Create a Multi from a list of Ints
+val multi = Multi.createFrom().items(1, 2, 3, 4)
+
+// Subscribing to a Multi
+val cancellable: Cancellable =
+    multi
+        .subscribe()
+        .with(
+            { println("Item $it") }, // Subscribing each item
+            { println("Failed with $it") }, // On Failure
+            { println("Completed") } // On Completion
+        )
+
+// Creating Multi from an Intstream
+val items1 = Multi.createFrom()
+    .items { IntStream.range(1, 10).boxed() }
+
+// Creating Multi from a collection
+val items2 = Multi.createFrom()
+    .iterable(listOf(1, 2, 3, 4, 5, 6))
+
+// Creating failing Multis
+val exception = Multi.createFrom().failure<Int>(Exception("Some Exception"))
+val exception2 = Multi.createFrom().failure<Int> { Exception("Another Exception") }
+
+// Creating empty Multis
+val empty = Multi.createFrom().empty<Int>()
+
+// Creating Multi from emitter
+val items: Multi<Int> = Multi.createFrom()
+    .emitter {
+        it.emit(1)
+        it.emit(2)
+        it.emit(3)
+        it.complete()
+    }
+
+// Creating Multi from ticks
+val ticks = Multi.createFrom()
+    .ticks().every(Duration.ofMillis(100))
